9d75bbc9f08392e8658e154fb1a266236286b73e
“Tomoki” <tomoki_26s1101554@nnn.ed.jp>
2026-09-14 18:05:58 +0900

    問題２

---
+++ q/Q2.py
@@ -0,0 +1,7 @@
+AveragescoreinJapaneselanguage= (85 + 90 + 65) // 3
+averagescoreinmathematics=(72 + 88 + 50) // 3
+averagescoreenglish=(89 + 95 + 72) // 3
+
+print(f"国語の平均点: {AveragescoreinJapaneselanguage}")
+print(f"数学の平均点: {averagescoreinmathematics}")
+print(f"英語の平均点: {averagescoreenglish}")
