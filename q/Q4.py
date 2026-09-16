8c061ab0cb7912d30a029c004fc32052ef14c84b
“Tomoki” <tomoki_26s1101554@nnn.ed.jp>
2026-09-16 18:01:07 +0900

    問題３

---
+++ q/Q4.py
@@ -0,0 +1,19 @@
+height = float(input(f"身長を入力してください(cm):"))
+weight = float(input(f"体重を入力してください(kg):"))
+
+height_m = height / 100
+
+bmi = weight / (height_m * height_m)
+
+bmi = round(bmi,1)
+
+print(f"BMI:",bmi)
+
+if bmi < 18.5:
+    print ("判定: 低体重")
+elif bmi < 25:
+    print ("判定: 普通体重")
+elif bmi < 30:
+    print ("判定: 肥満(1度)")
+else:
+    print ("判定: 肥満(2度以上)")
