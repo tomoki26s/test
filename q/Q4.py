25f37f504b3c832ed50119b9abc8aee46d4b4cb7
“Tomoki” <tomoki_26s1101554@nnn.ed.jp>
2026-09-16 18:06:26 +0900

    問題４

--- q/Q4.py
+++ q/Q4.py
@@ -5,15 +5,15 @@ height_m = height / 100
 
 bmi = weight / (height_m * height_m)
 
-bmi = round (bmi,1)
+bmi = round(bmi,1)
 
 print(f"BMI:",bmi)
 
 if bmi < 18.5:
     print ("判定: 低体重")
-elif bmi < 25:
+elif bmi < 25.0:
     print ("判定: 普通体重")
-elif bmi < 30:
+elif bmi < 30.0:
     print ("判定: 肥満(1度)")
 else:
     print ("判定: 肥満(2度以上)")
